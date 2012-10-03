4b4b61696150ce6559b6b7c75679841b26399081
Synergy Dev <synergycomputing@gmail.com>
2012-10-03 19:00:38 +0000

    Android.mk: compile with Os

--- Android.mk
+++ Android.mk
@@ -6,6 +6,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
+LOCAL_CFLAGS += -Os
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := SamsungServiceMode
 LOCAL_CERTIFICATE := platform
